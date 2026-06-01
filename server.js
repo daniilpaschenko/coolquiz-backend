9537a971e33e90280922554c8336ab45f085b67e
Daniil Paschenko <daniil_paschenko@mail.ru>
2026-06-01 16:23:18 +0700

    small fix

--- server.js
+++ server.js
@@ -6,27 +6,31 @@ const helmet = require('helmet');
 
 const authRoutes = require('./routes/auth');
 const quizRoutes = require('./routes/quizzes');
+const attemptRoutes = require('./routes/attempts');
 const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json()); // для json в запросах
 app.use(helmet());
 app.use(cors({
     origin: ['http://localhost:3000'],
     credentials: true,
 }));
+app.use(express.json()); // для json в запросах
 
 app.use('/api/auth', authRoutes);
 app.use('/api/quizzes', quizRoutes);
+app.use('/api', attemptRoutes);
 app.use(errorHandler); // обработчик ошибок
 
 const mongoUri = process.env.MONGO_URI; // строка подключения к MongoDB
 mongoose.connect(mongoUri)
     .then(() => console.log('🚀 Successfully connected to the CoolQuiz cloud MongoDB!'))
-    .catch(err => console.error('❌ Error connecting to the database:', err));
-    process.exit(1); // завершение процесса
+    .catch(err => {
+        console.error('❌ Error connecting to the database:', err);
+        process.exit(1);
+    });
 
 // запуск сервера на прослушивание порта
 app.listen(PORT, () => {
